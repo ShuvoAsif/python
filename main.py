613b3df66618071641e6f40d9db56f04d59c79ed
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-20 23:56:22 +0600

    input

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ isRich = True
 print(salary)
 print(person)
 print(isRich)
+
+# ------input------
+
+status = input("Are you single?: ")
+print("this person is", status)
+
+name = input("What's your name: ")
+age = int(input("What's your age: "))
+# fraction number
+salary = float(input("Your salary: "))
+
+print("all inputs are", name, age, salary)
