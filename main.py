cd7fdf47ea991a1cca92fe3d0cc0f420840806d0
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-21 14:47:04 +0600

    condition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# --------------------
 # ------variable------
+# --------------------
 
 # number
 salary = 1000
@@ -25,3 +27,27 @@ age = int(input("What's your age: "))
 salary = float(input("Your salary: "))
 
 print("all inputs are", name, age, salary)
+
+# ------condition------
+
+price = 1500
+if price < 1500:
+    print("then i will buy it")
+else:
+    print("i wont buy it")
+
+# and
+price1 = 9000
+storage = 1024
+if price < 5000 and storage > 512:
+    print("i will buy it")
+else:
+    print("i wont buy it")
+
+# or
+price1 = 7000
+storage = 126
+if price < 5000 or storage > 126:
+    print("i will buy it")
+else:
+    print("i wont buy it")
