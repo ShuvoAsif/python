a32d1ea6ad10345585424d57f768913bf077d14d
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-22 03:13:48 +0600

    functions

--- function.py
+++ function.py
@@ -9,3 +9,17 @@ def write():
 
 
 write()
+
+# parameter
+# return
+
+
+def multiply_double(num):
+    result = num * 2
+    return result
+
+
+first_bank_amount = multiply_double(15)
+second_bank_amount = multiply_double(35)
+total = first_bank_amount + second_bank_amount
+print('total money i have', total)
--- practice.py
+++ practice.py
@@ -1,7 +1,9 @@
-num = 0
+def multiply_double(num):
+    result = num * 2
+    return result
 
-while num <= 50:
-    num = num + 1
-    if num > 30:
-        break
-    print(num)
+
+first_bank_amount = multiply_double(15)
+second_bank_amount = multiply_double(35)
+total = first_bank_amount + second_bank_amount
+print('total money i have', total)
