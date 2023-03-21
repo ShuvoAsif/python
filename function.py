db384e12f5fbd9dbfd6b7f2335cc1b1fedffc679
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-22 02:53:44 +0600

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+# --------------------------------
+# ------------function------------
+# --------------------------------
+
+def write():
+    print("hello")
+    print("can you here me")
+    print("why don't you pick my phone")
+
+
+write()
