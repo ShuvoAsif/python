11e9482b31f363fef886c6b55ddc670cdb6df56a
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-24 23:19:27 +0600

    num

--- practice.py
+++ practice.py
@@ -1,8 +1,1 @@
-nums = [47, 49, 51, 31, 35, 39, 37]
-sum = 0
-avg = 0
-print('number of items', len(nums))
-for num in nums:
-    sum = sum + num
-    avg = sum / len(nums)
-print('avg of all numbers:', avg)
+num = 55
