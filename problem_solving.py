1f987a3e8d3a23d1238b1fc3df63701585ea3759
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-26 16:41:25 +0600

    smallest

--- problem_solving.py
+++ problem_solving.py
@@ -89,6 +89,14 @@ for num in nums:
 print('largest number you have', largest)
 
 # smallest
+nums = [43, 45, 55, 47, 59, 57, 77, 73, 79]
+smallest = nums[0]
+for num in nums:
+    print(smallest, num)
+    if num < smallest:
+        print('condition is true')
+        smallest = num
+print('smallest number you have', smallest)
 
 # -------count words in a sentence-------
 # -------number of vowels in a sentence-------
